68655ed5e0adb23ec7c4c27db2b5aca33ed823da
Daffa fauzan <daffafauzand24@gmail.com>
2022-12-27 00:58:28 +0700

    first commit

---
+++ booking_service/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav class="navbar">
+        <div class="left">
+            <h1>PT.Onder Jaya</h1>
+        </div>
+        <div class="right">
+            <ul class="list">
+                <li><a href="read_mobil.php">add service </a></li>
+                <li><a href="pending_service.php">pending service </a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
